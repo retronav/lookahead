19b7372380c377971e4be03d79add1a08bf5cfd4
Pranav Karawale <pdkarawale31@gmail.com>
2020-10-25 15:00:31 +0530

    chore: fix firebase app config for ci

--- packages/web/components/Firebase/firebaseConfig.ts
+++ packages/web/components/Firebase/firebaseConfig.ts
@@ -1,10 +1,17 @@
-export default {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_CONFIG_APIKEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_CONFIG_AUTHDOMAIN,
-  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_CONFIG_DATABASEURL,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_CONFIG_PROJECTID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_CONFIG_STORAGEBUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_CONFIG_MESSAGINGSENDERID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_CONFIG_APPID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_CONFIG_MESAUREMENTID,
-};
+const config = process.env.NEXT_PUBLIC_FIREBASE_APP_CONFIG
+  ? //CI
+    JSON.parse(process.env.NEXT_PUBLIC_FIREBASE_APP_CONFIG)
+  : //Local
+    {
+      apiKey: process.env.NEXT_PUBLIC_FIREBASE_CONFIG_APIKEY,
+      authDomain: process.env.NEXT_PUBLIC_FIREBASE_CONFIG_AUTHDOMAIN,
+      databaseURL: process.env.NEXT_PUBLIC_FIREBASE_CONFIG_DATABASEURL,
+      projectId: process.env.NEXT_PUBLIC_FIREBASE_CONFIG_PROJECTID,
+      storageBucket: process.env.NEXT_PUBLIC_FIREBASE_CONFIG_STORAGEBUCKET,
+      messagingSenderId:
+        process.env.NEXT_PUBLIC_FIREBASE_CONFIG_MESSAGINGSENDERID,
+      appId: process.env.NEXT_PUBLIC_FIREBASE_CONFIG_APPID,
+      measurementId: process.env.NEXT_PUBLIC_FIREBASE_CONFIG_MESAUREMENTID,
+    };
+
+export default config;
