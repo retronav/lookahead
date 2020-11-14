52719e3c61f1c4a4cdbfa6d142b537798d7d8bda
Pranav Karawale <pdkarawale31@gmail.com>
2020-11-14 11:13:05 +0530

    chore: use loader with transparent background

--- packages/web/components/Application/LazyLoader.tsx
+++ packages/web/components/Application/LazyLoader.tsx
@@ -2,18 +2,20 @@ import React from "react";
 import isBrowser from "../util/isBrowser";
 const mode = isBrowser() && window.localStorage.getItem("themeName");
 
-const LazyLoader = () => <video
-  style={{
-    position: "absolute",
-    top: "50%",
-    left: "50%",
-    transform: "translate(-50%, -50%)",
-    ...(mode === "dark" ? { filter: "invert(0.795) brightness(1.25)" } : {}),
-  }}
-  loop
-  autoPlay
-  muted
-  src="/loader.webm"
-/>;
+const LazyLoader = () => (
+  <video
+    style={{
+      position: "absolute",
+      top: "50%",
+      left: "50%",
+      transform: "translate(-50%, -50%)",
+      ...(mode === "dark" ? { filter: "invert(1)" } : {}),
+    }}
+    loop
+    autoPlay
+    muted
+    src="/loader.webm"
+  />
+);
 
 export default LazyLoader;
