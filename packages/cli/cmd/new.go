b2411144dc98ef0c0cc487e9621959c781ce294e
Pranav Karawale <pdkarawale31@gmail.com>
2020-12-08 15:48:35 +0530

    feat(cli): support multiline content via flags

--- packages/cli/cmd/new.go
+++ packages/cli/cmd/new.go
@@ -16,6 +16,8 @@ limitations under the License.
 package cmd
 
 import (
+	"strings"
+
 	"github.com/spf13/cobra"
 	"lookahead.web.app/cli/internal/input"
 	"lookahead.web.app/cli/internal/logging"
@@ -25,14 +27,18 @@ import (
 // newCmd represents the new command
 var newCmd = &cobra.Command{
 	Use:   "new",
-	Short: "Create a new todo.",
-	Long: `Create a new todo using this command.
+	Short: "Create a new todo/note.",
+	Long: `Create a new todo/note using this command.
 	
-	To create a new todo in interactive mode, run 'look new'.
-	To directly create a todo using flags, run 
-	'look new -t "YOUR TITLE" -c "YOUR CONTENT HERE"'.
-	The method of creating todos using 
-	command line flags don't support multiline content.`,
+To create a new todo/note in interactive mode, run 'look new'.
+To directly create a todo/note using flags, run 
+'look new -t "YOUR TITLE" -c "YOUR CONTENT HERE"'.
+If you need to insert newlines in the content simply use "\n", for example:
+look new -t "Title" -c "Multiline\nWorks"
+
+Flags:
+-t, --title : Set the title of the new todo/note
+-c, --content: Set the content of the new todo/note`,
 	PreRun: func(cmd *cobra.Command, args []string) {
 		title, _ := cmd.Flags().GetString("title")
 		content, _ := cmd.Flags().GetString("content")
@@ -48,20 +54,24 @@ var newCmd = &cobra.Command{
 		if title == "" {
 			title = input.Input("Enter the new title: ")
 			// Because title can't be empty
-			for []byte(title)[0] == /*Newline*/ 13 {
+			for title == "" {
 				title = input.Input("Enter the new title: ")
 			}
 			content = input.MultilineInput("Enter the new content: ")
 		}
+		content = strings.Replace(content, "\\n", "\n", -1)
 		s := logging.DarkSpinner(" Creating new todo/note")
 		s.Start()
-		err := store.Store.Append(title, content)
+		isOffline, err := store.Store.Append(title, content)
 		if err != nil {
 			s.Stop()
 			logging.Error(1, err.Error())
 		} else {
-
 			s.Stop()
+			if isOffline {
+				logging.Warn("Couldn't sync to the online store" +
+					" because you seem to be offline.")
+			}
 			logging.Success("Created successfully!!")
 		}
 	},
@@ -70,7 +80,6 @@ var newCmd = &cobra.Command{
 func init() {
 	rootCmd.AddCommand(newCmd)
 
-	newCmd.Flags().StringP("title", "t", "", "Set the title ")
-	newCmd.Flags().StringP("content", "c", "", "Set the content."+
-		" Doesn't support multiline ")
+	newCmd.Flags().StringP("title", "t", "", "Set the title of the new todo/note")
+	newCmd.Flags().StringP("content", "c", "", "Set the content of the new todo/note")
 }
