bc1cdf56d73c726f16a04c29c3546b801d2d96f7
Pranav Karawale <pdkarawale31@gmail.com>
2021-03-18 11:41:49 +0530

    chore(web): add correct types to views

--- packages/web/src/index.ts
+++ packages/web/src/index.ts
@@ -1,5 +1,9 @@
 import './components/app-navbar';
 import { Router } from '@vaadin/router';
+import type { AppHome } from './views/app-home';
+import type { AppSignIn } from './views/app-signin';
+import type { AppDash } from './views/app-dash';
+import type { App404 } from './views/app-404';
 
 const appRoot = document.querySelector('app-root') as HTMLElement;
 export const router = new Router(appRoot);
@@ -7,23 +11,27 @@ export const router = new Router(appRoot);
 router.setRoutes([
   {
     path: '/',
-    action: async () => (await import('./views/app-home')) as any,
+    action: async () =>
+      ((await import('./views/app-home')) as unknown) as AppHome,
     component: 'app-home',
   },
   {
     path: '/signin',
-    action: async () => (await import('./views/app-signin')) as any,
+    action: async () =>
+      ((await import('./views/app-signin')) as unknown) as AppSignIn,
     component: 'app-signin',
   },
   {
     path: '/app',
-    action: async () => (await import('./views/app-dash')) as any,
+    action: async () =>
+      ((await import('./views/app-dash')) as unknown) as AppDash,
     component: 'app-dash',
   },
   // Not Found Fallback
   {
     path: '/(.*)',
-    action: async () => (await import('./views/app-404')) as any,
+    action: async () =>
+      ((await import('./views/app-404')) as unknown) as App404,
     component: 'app-404',
   },
 ]);
